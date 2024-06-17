7430b9fac29aa7eaa93ee31ae0b78da1e0c192ad
Sai <saidcooldude@gmail.com>
2024-06-17 17:53:53 -0500

    adding heap challenge

--- main.c
+++ main.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+typedef struct string {
+    unsigned length;
+    char *data;
+} string;
 
 int main() {
-    int secret = 0xdeadbeef;
-    char name[100] = {0};
-    read(0, name, 0x100);
-    if (secret == 0x1337) {
-        puts("Wow! Here's a secret.");
-    } else {
-        puts("I guess you're not cool enough to see my secret");
-    }
+    struct string* s = malloc(sizeof(string));
+    puts("Length:");
+    scanf("%u", &s->length);
+    s->data = malloc(s->length + 1);
+    memset(s->data, 0, s->length + 1);
+    puts("Data:");
+    read(0, s->data, s->length);
+
+    free(s->data);
+    free(s);
+
+    char *s2 = malloc(16);
+    memset(s2, 0, 16);
+    puts("More data:");
+    read(0, s2, 15);
+
+    // Now using s again, a UAF
+
+    puts(s->data);
+
+    return 0;
 }
