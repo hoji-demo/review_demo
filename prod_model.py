fe9b98fa9f91919c3a1f454128f9a746ec924674
Sai <saidcooldude@gmail.com>
2024-06-18 09:09:41 -0500

    adding a production grade mnist model

--- prod_model.py
+++ prod_model.py
@@ -15,13 +15,13 @@ y_test = to_categorical(y_test)
 
 model = Sequential()
 model.add(Flatten(input_shape=(28, 28)))
-model.add(Dense(512, activation='relu'))  
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.2))
-model.add(Dense(256, activation='relu')) 
+model.add(Dense(256, activation='relu'))
 model.add(Dropout(0.2))
 model.add(Dense(10, activation='softmax'))
 
-optimizer = Adam(learning_rate=0.001)  
+optimizer = Adam(learning_rate=0.001)
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
@@ -29,4 +29,3 @@ model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f"Test Loss: {loss}")
 print(f"Test Accuracy: {accuracy}")
-
