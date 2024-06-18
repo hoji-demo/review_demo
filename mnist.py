d0f1a95047f8a7a424b4f95477fb30bcb67a9d3a
Sai <saidcooldude@gmail.com>
2024-06-18 09:05:40 -0500

    removing dodgy comments

--- mnist.py
+++ mnist.py
@@ -5,7 +5,6 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Dropout
 from tensorflow.keras.optimizers import Adam
 
-# Load and preprocess data
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train / 255.0
@@ -14,23 +13,19 @@ x_test = x_test / 255.0
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-# Define the model (incorrectly)
 model = Sequential()
 model.add(Flatten(input_shape=(28, 28)))
-model.add(Dense(512, activation='relu'))  # Using too many neurons
+model.add(Dense(512, activation='relu'))  
 model.add(Dropout(0.2))
-model.add(Dense(256, activation='relu'))  # Adding another layer with too many neurons
+model.add(Dense(256, activation='relu')) 
 model.add(Dropout(0.2))
 model.add(Dense(10, activation='softmax'))
 
-# Compile the model with a non-standard optimizer
-optimizer = Adam(learning_rate=0.001)  # Standard optimizer, but let's use a very low learning rate
+optimizer = Adam(learning_rate=0.001)  
 model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model (with a low number of epochs)
 model.fit(x_train, y_train, epochs=10, batch_size=32, validation_data=(x_test, y_test))
 
-# Evaluate the model on the test set
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f"Test Loss: {loss}")
 print(f"Test Accuracy: {accuracy}")
