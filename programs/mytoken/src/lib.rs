5417f4164e69a62de4df9a770f93bffcbb0fc28e
Sai <saidcooldude@gmail.com>
2024-07-05 11:58:16 -0500

    demo prep

--- programs/mytoken/src/lib.rs
+++ programs/mytoken/src/lib.rs
@@ -12,7 +12,7 @@ pub mod mytoken {
         let cpi_accounts = token::MintTo {
             mint: ctx.accounts.mint.to_account_info(),
             to: ctx.accounts.token_account.to_account_info(),
-            authority: ctx.accounts.user.to_account_info(), // Vulnerable: using user as mint authority
+            authority: ctx.accounts.user.to_account_info(), 
         };
         let cpi_program = ctx.accounts.token_program.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
@@ -26,7 +26,7 @@ pub mod mytoken {
         let cpi_accounts = Transfer {
             from: ctx.accounts.from.to_account_info(),
             to: ctx.accounts.to.to_account_info(),
-            authority: ctx.accounts.authority.to_account_info(), // Vulnerable: any signer can authorize transfer
+            authority: ctx.accounts.authority.to_account_info(), // 
         };
         let cpi_program = ctx.accounts.token_program.to_account_info();
         let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
@@ -38,12 +38,12 @@ pub mod mytoken {
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(init, payer = user, mint::decimals = 9, mint::authority = user)] // Vulnerable: user as mint authority
+    #[account(init, payer = user, mint::decimals = 9, mint::authority = user)] 
     pub mint: Account<'info, Mint>,
-    #[account(init, payer = user, token::mint = mint, token::authority = user)] // Vulnerable: user as token authority
+    #[account(init, payer = user, token::mint = mint, token::authority = user)] 
     pub token_account: Account<'info, TokenAccount>,
     #[account(mut)]
-    pub user: Signer<'info>, // Vulnerable: unchecked authority
+    pub user: Signer<'info>, 
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
     pub rent: Sysvar<'info, Rent>,
@@ -55,7 +55,7 @@ pub struct TransferTokens<'info> {
     pub from: Account<'info, TokenAccount>,
     #[account(mut)]
     pub to: Account<'info, TokenAccount>,
-    pub authority: Signer<'info>, // Vulnerable: unchecked signer
+    pub authority: Signer<'info>, 
     pub token_program: Program<'info, Token>,
 }
 
